9a3c0cb5d70778009b0ce83f284c9358cd5b2a0b
peter001 <kaopeter@gmail.com>
2017-01-01 18:55:41 +0800

    add docs

--- lib/selectize.dart
+++
@@ -1,8 +0,0 @@
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-library selectize;
-
-export 'src/base.dart';
-export 'src/configuration.dart';
-export 'src/osSelectize.dart';
